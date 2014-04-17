0013032905a12d6bfde6582f7dc71fb9204afe58
Jean-Marie RENOUARD <jmrenouard@gmail.com>
2014-04-17 22:17:57 +0200

    Adding Licence Headers for all files

--- sample_build.sh
+++ sample_build.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
+#------------------------------------------------------------------------------
+#
+# MyRPM - Rpm Utilities
+# Copyright (c) Jean-Marie RENOUARD 2014 - LightPath
+# Contact : Jean-Marie Renouard <jmrenouard at gmail.com>
+#
+# This program is open source, licensed under the Artistic Licence v2.0.
+#
+# Artistic Licence 2.0
+# Everyone is permitted to copy and distribute verbatim copies of 
+# this license document, but changing it is not allowed.
+#
+#------------------------------------------------------------------------------
 
 #APPLI CONFIG
-APPLI=socle-ciweb2g
+APPLI=myrpm
 VERSION=2.1
 RELEASE=6
-CHANGELOG="Ajustement parametres JVM - VERSION DE PRODUCTION"
-REQUIRES="ciweb2g-jakarta-tomcat,ciweb2g-jrockit"
+CHANGELOG="RELEASE 2.1 - VERSION DE PRODUCTION"
+REQUIRES="jakarta-tomcat,jrockit"
 EXCLUDE='build.sh,post.sh,pre.sh,\.rpm$' 
 
 . /usr/bin/rpmFactory
